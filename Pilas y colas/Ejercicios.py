fe4c83ec0ee873dcbf4e9b455371c88ef6e8eab3
Sanet.Coding <mancmancra@gmail.com>
2026-04-17 12:40:46 +0000

    Pilas Y colas ejercicios

--- Pilas y colas/Ejercicios.py
+++ Pilas y colas/Ejercicios.py
@@ -2,12 +2,13 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__))
 
-from Queue import Queue, QueueNode
+from Queue import Queue, QueueNode, Stack
 
-custom_queue = Queue()
+
+'''custom_queue = Queue()
 custom_queue.generate(10, 1, 50)
 print("Cola generada:")
-print(custom_queue)
+print(custom_queue)'''
 
 '''def fix_string(input_str):
     corrected_str = Queue()
@@ -27,4 +28,183 @@ print(custom_queue)
     corrected_str.inQueue(input_str[index])'''
 
 
+'''def validate_expresion(math_expr):
+    open= "{(["
+    close= "})]"
+    aux_stack = Stack()
+    for c in math_expr:
+        if c in open:
+            aux_stack.push(c)
+        elif c in close:
+            print ("operador de cierre: ", c)
+            print(" indice operador de cierre: ", close.index(c))
+            if not aux_stack.is_empty():
+                last_open = aux_stack.pop()
+            else:
+                return False
+            print ("ultimo operador de apertura: ", last_open)
+            print(" indice operador de apertura: ", open.index(last_open))
+            if open.index(last_open) != close.index(c):
+                return False
+    return aux_stack.is_empty()
 
+
+exp1 = "({[]})"
+exp2 = "({[})]"
+exp3 = "({[]})]"
+exp4 = "({[]}"
+
+print(validate_expresion(exp1))
+print(validate_expresion(exp2))
+print(validate_expresion(exp3))
+print(validate_expresion(exp4))'''
+
+'''class QueueStack:
+
+    def __init__(self):
+        self.in_stack = Stack()
+        self.aux_stack = Stack()
+
+    def inQueue(self, elemento):
+        self.in_stack.push(elemento)
+
+    def deQueue(self):
+        if self.is_empty():
+            return 'No hay elementos en la cola'
+        else:
+            while not self.in_stack.is_empty():
+                self.aux_stack.push(self.in_stack.pop())
+            
+
+    def is_empty(self):
+        return self.in_stack.is_empty() and self.aux_stack.is_empty()
+    
+    def len(self):
+        return self.in_stack.size + self.aux_stack.size
+    
+    def __str__ (self):
+        result = []
+        current = self.aux_stack.top
+        while current is not None:
+            result.append(str(current.dato))
+            current = current.next
+        current = self.in_stack.top
+        while current is not None:
+            result.append(str(current.dato))
+            current = current.next
+        return ' -- '.join(result)
+'''
+
+'''def change_positions(stk,i,j):
+    aux_stack = Stack()
+    index = stk.len() - 1
+    value_i = ''
+    value_j = ''
+    while not stk.is_empty():
+        temp_value = stk.pop()
+
+        if index == i:
+            value_i = temp_value
+        if index == j:
+            value_j = temp_value
+        else:
+
+        
+        aux_stack.push(temp_value)
+        index -= 1
+
+
+custom_stack = Stack()
+custom_stack.generate_random_stack(10)
+
+print("Pila generada:", custom_stack)
+print("Pila con posiciones 2 y 5 intercambiadas:", change_positions(custom_stack, 2, 5))'''
+
+'''def put_in_queue(queue, element, i):
+    aux_queue = queue
+    index = 0
+    result= Queue()
+    if i < 0 or i >= queue.size:
+        raise IndexError("La posición es inválida")
+
+    while not queue.is_empty():
+        temp_value = queue.deQueue()
+        if index == i:
+            result.inQueue(element)
+        else:
+            result.inQueue(temp_value)
+        index += 1
+    return result 
+
+custom_queue = Queue()
+custom_queue.generate(10)
+print("Cola generada:", custom_queue)
+print("Cola con elemento 99 en la posición 3:", put_in_queue(custom_queue, 99, 3))'''
+
+'''stack1 = Stack()
+stack2 = Stack()
+stack3 = Stack()
+stack4 = Stack()
+
+stack1.generate_random_stack(4)
+stack2.generate_random_stack(4)
+stack3.generate_random_stack(4)
+stack4.generate_random_stack(4)
+
+print("stack 1:", stack1)
+print("stack 2:", stack2)
+print("stack 3:", stack3)
+print("stack 4:", stack4)
+
+stack_of_stacks = Stack()
+stack_of_stacks.push(stack1)
+stack_of_stacks.push(stack2)
+stack_of_stacks.push(stack3)
+stack_of_stacks.push(stack4)
+
+def find_min(stack_of_stacks):
+    Q_result = Queue()
+    while not stack_of_stacks.is_empty():
+        temp_stack = stack_of_stacks.pop()
+        min_value =temp_stack.top.dato
+        while not temp_stack.is_empty():
+            temp_value = temp_stack.pop()
+            if temp_value < min_value:
+                min_value = temp_value
+        Q_result.inQueue(min_value)
+    return Q_result
+
+print("Q_result:", find_min(stack_of_stacks))'''
+
+stack = Stack()
+stack.generate_random_stack(10)
+print("stack: ", stack)
+
+def organize_queue_min_Max(stack):
+    result_stack = Stack()
+    while not stack.is_empty():
+        current_value = stack.pop()
+
+        if current_value is None:
+            break
+
+        if result_stack.is_empty():
+            result_stack.push(current_value)
+        elif result_stack.top.dato < current_value:
+            result_stack.push(current_value)
+        elif result_stack.top.dato > current_value:
+            while not result_stack.is_empty() and result_stack.top.dato >= current_value:
+                stack.push(result_stack.pop())
+            result_stack.push(current_value)
+        else:
+            result_stack.push(current_value)
+
+    while not result_stack.is_empty():
+        stack.push(result_stack.pop())
+    return stack
+
+print('stack_result: ', organize_queue_min_Max(stack))
+        
+
+        
+        
--- Pilas y colas/Queue.py
+++ Pilas y colas/Queue.py
@@ -21,7 +21,7 @@ class Queue:
     else:
       self.tail.next = nodo
       self.tail = nodo
-    self._size += 1
+    self.size += 1
 
   def deQueue(self):
     if self.head is None:
@@ -30,8 +30,16 @@ class Queue:
     self.head = nodo.next
     if self.head is None:
       self.tail = None
-    self._size -= 1
+    self.size -= 1
     return nodo.dato
+  
+  def is_empty(self):
+    return self.size == 0
+  
+  def generate(self, n, min_val=1, max_val=100):
+    import random
+    for _ in range(n):
+      self.inQueue(random.randint(min_val, max_val))
   
 
   def __str__(self):
@@ -41,3 +49,40 @@ class Queue:
       result.append(str(current.dato))
       current = current.next
     return ' -- '.join(result)
+
+
+class Stack:
+
+  def __init__(self):
+    self.top = None
+    self.size = 0
+
+  def push(self, elemento):
+    nodo = QueueNode(elemento)
+    nodo.next = self.top
+    self.top = nodo
+    self.size += 1
+
+  def pop(self):
+    if self.top is None:
+      return None
+    nodo = self.top
+    self.top = nodo.next
+    self.size -= 1
+    return nodo.dato
+  
+  def __str__(self):
+    result = []
+    current = self.top
+    while current is not None:
+      result.append(str(current.dato))
+      current = current.next
+    return ' | '.join(result)
+
+  def is_empty(self):
+    return self.size == 0
+  
+  def generate_random_stack(self, n):
+    import random
+    for _ in range(n):
+      self.push(random.randint(1, 100))
